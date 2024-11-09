2266155ac4bf0096666481c7b259a21ae7ac8e51
z5d69 <44180425+z5d69@users.noreply.github.com>
2024-11-08 20:25:42 -0600

    Update install_neo4j.sh
    Added autoapprove / silent.

--- install_neo4j.sh
+++ install_neo4j.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 -- JRE
-yum install https://builds.openlogic.com/downloadJDK/openlogic-openjdk-jre/22.0.2+9/openlogic-openjdk-jre-22.0.2+9-linux-x64-el.rpm
+yum -y install https://builds.openlogic.com/downloadJDK/openlogic-openjdk-jre/22.0.2+9/openlogic-openjdk-jre-22.0.2+9-linux-x64-el.rpm
 -- Neo4j Java adapter 
-yum install https://dist.neo4j.org/neo4j-java17-adapter.noarch.rpm
+yum -y install https://dist.neo4j.org/neo4j-java17-adapter.noarch.rpm
 -- Adding Neo4j Repo which is compatible with RHEL9, you can just populate the repo file directly
 rpm --import https://debian.neo4j.com/neotechnology.gpg.key
 cat << EOF >  /etc/yum.repos.d/neo4j.repo
@@ -15,9 +15,9 @@ EOF
 -- Installing Neo4j 5+
 Either 
 --interactive
-yum install neo4j-enterprise-5.25.1
+yum -y install neo4j-enterprise-5.25.1
 --non-interactive
-NEO4J_ACCEPT_LICENSE_AGREEMENT=yes yum install neo4j-enterprise-5.25.1
+NEO4J_ACCEPT_LICENSE_AGREEMENT=yes yum -y install neo4j-enterprise-5.25.1
 -- Enable as a service for reboots
 systemctl enable neo4j
 -- Start as a service for reboots
